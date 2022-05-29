b86aefe5b4ac858f5fff6816ab3dc5735cd6d12d
LukasGaspierik <72013256+LukasGaspierik@users.noreply.github.com>
2022-05-29 22:34:11 +0200

    main

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'><h1>HYPESTORE</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/shoes' activeStyle>Shoes</NavLink>
+          <NavLink to='/clothing' activeStyle>Clothing</NavLink>
+          <NavLink to='/accessories' activeStyle>Accessories</NavLink>
+          <NavLink to='/signup' activeStyle>Sign up</NavLink>
+        </NavMenu>
+        <NavBtn>
+        <NavBtnLink to='/'>Add Product</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
